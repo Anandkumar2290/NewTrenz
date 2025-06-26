7166201878bb63610116011165d7d3de3150797d
Anandkumar2290 <anandkumar.mondyagu2290@gmail.com>
2025-06-26 09:59:41 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+In this project, let's build a **Nxt Trendz**
+
+### Refer to the video below:
+
+<br/>
+<div style="text-align: center;">
+  <video style="max-width:70%;box-shadow:0 2.8px 2.2px rgba(0, 0, 0, 0.12);outline:none;" loop="true" autoplay="autoplay" controls="controls" muted>
+    <source src="https://assets.ccbp.in/frontend/content/react-js/nxt-trendz-cart-features-output.mp4" type="video/mp4">
+  </video>
+</div>
+<br/>
+
+<details>
+
+- Prime User credentials
+
+  ```text
+   username: rahul
+   password: rahul@2021
+  ```
+
+- Non-Prime User credentials
+
+  ```text
+   username: raja
+   password: raja@2021
+  ```
+
+</details>
+
---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,31 @@
+// Write your code here
+
+import CartContext from '../../context/CartContext'
+import './index.css'
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      let totalAmout = 0
+      for (const eachItem of cartList) {
+        totalAmout += eachItem.price * eachItem.quantity
+      }
+      return (
+        <div className="cart-summary-container">
+          <h1 className="cart-summary-heading">
+            Order Total:{' '}
+            <span className="total-amount"> Rs {totalAmout}/- </span>
+          </h1>
+          <p className="cart-quantity">{cartList.length} Items in cart </p>
+          <button type="button" className="checkout-button">
+            {' '}
+            Checkout{' '}
+          </button>
+        </div>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default CartSummary
